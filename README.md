ec30480a39cb55e4c05a60b4cdef0a2e2eaa529c
Logan Bleyl <loganmbleyl05@gmail.com>
2025-11-30 20:45:11 -0500

    Shoot rate and asteroid destruction

---
+++ README.md
@@ -0,0 +1,1 @@
+Asteroids game Boot.dev project.
--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cd_timer = 0
    
     # in the Player class
     def triangle(self):
@@ -25,6 +26,9 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_cd_timer -= dt
+        if self.shoot_cd_timer < 0:
+            self.shoot_cd_timer = 0
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -44,6 +48,9 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+        if self.shoot_cd_timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         direction = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = direction * PLAYER_SHOOT_SPEED
+        self.shoot_cd_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
